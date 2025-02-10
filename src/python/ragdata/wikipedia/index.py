376a00f64f3590379f4de95672d1e09c63a7f892
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2025-02-10 16:38:21 -0500

    Improve parsing of Wikipedia articles with leading formatting boxes, closes #1

--- src/python/ragdata/wikipedia/index.py
+++ src/python/ragdata/wikipedia/index.py
@@ -40,7 +40,7 @@ class Reader(ReaderBase):
 
         # Titles and abstract prefixes to skip
         skiptitles = ["List of ", "Timeline of ", "Timelines "]
-        skipabstracts = ["Events ", "The following events "]
+        skipabstracts = ["Events ", "The following events ", "REDIRECT "]
 
         # Batch of rows
         batch = []
@@ -50,21 +50,17 @@ class Reader(ReaderBase):
             title = row["title"]
 
             # First text section is considered abstract
-            abstract = row["text"].split("\n\n")[0]
-
-            # Cleanup empty parens
-            abstract = re.sub(r" \(\s*\)", "", abstract)
+            abstract = self.abstract(row["text"])
 
             # Accept article using following rules
             # - title does not contain 'disambiguation' and does not start with skip title prefixes
-            # - abstract is not empty and does not start with skip abstract prefixes and does not end with ':'
+            # - abstract is not empty and does not end with ':'  and does not start with skip abstract prefixes
             # - lede does not contain 'can refer to' or 'may refer to'
             if (
                 "disambiguation" not in title
                 and not any(title.startswith(p) for p in skiptitles)
-                and abstract.strip()
+                and abstract and not abstract.endswith(":")
                 and not any(abstract.startswith(p) for p in skipabstracts)
-                and not abstract.strip().endswith(":")
             ):
                 # Split into sentences
                 lede = sent_tokenize(abstract)[0]
@@ -110,6 +106,40 @@ class Reader(ReaderBase):
             rank[title] = score
 
         return rank
+
+    def abstract(self, text):
+        """
+        Builds an abstract from article text. The first paragraph with text is used as the abstract.
+
+        Args:
+            text: article text
+
+        Returns:
+            abstract text
+        """
+
+        # Detect and remove formatting boxes
+        formatbox = ("{", "}", "|", "[", "]", "!")
+        if text.strip().startswith(formatbox):
+            # Remove info boxes
+            text = re.sub(r"\{[{|\n].*[}|\n]\}", "", text, flags=re.DOTALL)
+
+            # Remove attachment links
+            text = re.sub(r"\[.*\]", "", text, flags=re.DOTALL)
+
+            # Filter lines that start with a formatting box character - handles partial formatting boxes
+            text = "\n".join(x for x in text.split("\n") if not x.strip().startswith(formatbox))
+
+            # Filter empty and low token sections parsed
+            text = "\n\n".join(x for x in text.split("\n\n") if x.strip() and len(x.split()) >= 5)
+
+        # The first/lede section in the article is considered the abstract
+        sections = text.split("\n\n")
+        abstract = sections[0].strip() if sections else ""
+
+        # Cleanup empty parens
+        abstract = re.sub(r" \(\s*\)", "", abstract)
+        return abstract
 
     def percentile(self, rank, title):
         """
